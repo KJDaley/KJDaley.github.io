d0e40e5813e3476dafc8508d5cc63c3ba345325a
KJ Daley <42543425+KJDaley@users.noreply.github.com>
2018-12-04 15:13:36 -0600

    Add files via upload

---
+++ Javascript5Assignment/Javascript5Assignment.js
@@ -0,0 +1,18 @@
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor1 = "X coords: " + x;
+    var coor2 = "Y coords: " + y;
+    document.getElementById("uno").innerHTML = coor1;
+    document.getElementById("dos").innerHTML = coor2;
+}
+
+function setZero(e) {
+  if (e.keyCode == 67)
+  {
+    document.getElementById("uno").innerHTML = "Y coords: "+ 0;
+    document.getElementById("dos").innerHTML ="Y coords: " + 0;
+  }
+}
+
+document.addEventListener('keydown', setZero, false);
